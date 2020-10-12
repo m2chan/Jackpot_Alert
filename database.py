e4752e9aaaf60197746b504e8408d8ce8201049c
Marco Chan <marcochan@Marcos-MacBook-Pro.local>
2020-10-12 19:56:52 -0400

    Updated database

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
-# Create subscribers database\
+# Create subscribers database
 
 import sqlite3
 from sqlite3 import Error
 
-
 def create_connection(db_file):
     '''
     Create a database connection to the SQLite subscribers database
+    
+    Input arguments:
+        db_file: Database name we are connecting to
     '''
 
     conn = None
@@ -33,7 +35,7 @@ def create_table(conn, create_table_sql):
     except Error as e:
         print(e)
 
-def create_subscriber(conn, subscriber):
+def create_subscriber(conn, subscriber_data):
     '''
     Adds a new subscriber to the database
 
@@ -47,13 +49,14 @@ def create_subscriber(conn, subscriber):
     '''
 
     cur = conn.cursor()
-    cur.execute(sql, subscriber)
+    cur.execute(sql, subscriber_data)
     conn.commit()
     return cur.lastrowid
 
-def main():
+if __name__ == '__main__':
     database = 'subscribers.db'
-
+    
+    # Initialize database with these rows
     sql_create_subscribers_table = ''' CREATE TABLE IF NOT EXISTS subscribers (
                                             id integer PRIMARY KEY,
                                             email text,
@@ -72,12 +75,13 @@ def main():
         create_table(conn, sql_create_subscribers_table)
     
     with conn:
+<<<<<<< Updated upstream
         sub_1 = ('name1@email.com', '4162003000', 1, 5, 1, 30, 1)
         sub_2 = ('name2@email.com', '4161002000', 1, 10, 1, 50, 1)
+=======
+        sub_1 = ('marco.chan30@gmail.com', '4162001611', 1, 1, 1, 1, 1)
+>>>>>>> Stashed changes
 
         create_subscriber(conn, sub_1)
-        create_subscriber(conn, sub_2)
-
-if __name__ == '__main__':
-    main()
+        # create_subscriber(conn, sub_2)
 
